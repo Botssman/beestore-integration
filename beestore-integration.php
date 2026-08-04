3ddd86355d21c20b91120d309a7b40d8ce2f79dd
BeeStore Integration <beestore-integration@bot.local>
2026-08-04 05:30:13 +0000

    v1.7.9: EMERGENCY REVERT to v1.7.5 (stable)
    v1.7.6, v1.7.7, v1.7.8 caused White Screen of Death on user site.
    
    Reverted to v1.7.5 codebase, only bumped version to 1.7.9 so WordPress
    will offer update from broken v1.7.8 to working v1.7.5-equivalent.
    
    User should update to v1.7.9 to fix the white screen.

--- beestore-integration.php
+++ beestore-integration.php
@@ -3,7 +3,7 @@
  * Plugin Name:       BeeStore Integration for WooCommerce
  * Plugin URI:        https://github.com/Botssman/beestore-integration
  * Description:       Интеграция WooCommerce с BeeStore (Sirio Informatica): импорт каталога из CSV-выгрузки FTP, синхронизация остатков, передача заказов через SOAP (fInserimentoPrenotazione), обратная синхронизация статусов и tracking number, отмена/доплата заказа, конвертация цен и картинок в WebP, гибкие фильтры импорта по категориям и брендам.
- * Version:           1.7.5
+ * Version:           1.7.9
  * Author:            Kirill Andreev
  * License:           GPL-2.0+
  * Text Domain:       beestore-integration
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /* -------------------------------------------------------------------------
  * Константы плагина
  * ------------------------------------------------------------------------- */
-define( 'BSI_VERSION', '1.7.5' );
+define( 'BSI_VERSION', '1.7.9' );
 define( 'BSI_PLUGIN_FILE', __FILE__ );
 define( 'BSI_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'BSI_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
